3b7a8355cc790d986f7c73259aa99bc39afb1e42
Pushpanjalibot <reshulal19@gmail.com>
2025-04-21 11:28:48 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+    
+    cout << "Simple Calculator" << endl;
+    cout << "----------------" << endl;
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+    
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+    switch(operation) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else
+                cout << "Error! Division by zero!";
+            break;
+        default:
+            cout << "Invalid operation!";
+    }
+    
+    cout << endl;
+    return 0;
+}
